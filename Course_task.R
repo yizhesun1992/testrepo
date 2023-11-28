1e8b721e5a625d777fc65cf08cb429aad97eb9cf
Yizhe Sun <yizhe.sun@ki.se>
2023-11-29 00:08:12 +0100

    Create a new file

---
+++ Course_task.R
@@ -0,0 +1,33 @@
+library(renv)
+renv::init()
+renv::install(c("ggplot2", "dplyr", "tidyr"))
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+
+# Create a dataset with additional variables
+set.seed(456)
+data <- data.frame(
+  x = rnorm(100),
+  category = sample(c("A", "B", "C"), 100, replace = TRUE)
+) %>% mutate(y = 2 * x + rnorm(100))
+
+
+# Perform data manipulation using tidyverse
+data_summary <- data %>%
+  group_by(category) %>%
+  summarise(
+    mean_x = mean(x),
+    sd_y = sd(y),
+    n = n()
+  )
+renv::deactivate()
+renv::activate()
+
+renv::snapshot()
+
+renv::view_libraries()
+renv::lockfile_read()
+
+
